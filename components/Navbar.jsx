6548959397d141a344be6d375fbe28a4fe968c4f
Gurpreet <gs109690@gmail.com>
2022-12-31 03:04:05 -0600

    Added basic tailwind css to heading in Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,10 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Nav</h1>
+    <div className='fixed w-full h-25 shadow-xl z-[100]'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+         <h1 className=""> Navigation </h1>
+        </div> 
     </div>
   )
 }
