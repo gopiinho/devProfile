392270728b9ca91b941c454f568a0215299f4f73
Gurpreet <gs109690@gmail.com>
2023-01-01 23:08:55 -0600

    Imported react icons to use for smaller screens

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react"
+import Link from "next/link"
+import { BiCode } from "react-icons/bi"
+import { AiOutlineClose } from "react-icons/ai"
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-25 shadow-xl z-[100]'>
-        <div className='flex justify-between items-center w-full h-full p-4 2xl:px-16'>
-         <h1 className=""> Navigation </h1>
-         <div>
-          <ul className='hidden md:flex'>
+    <div className="fixed w-full h-25 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full p-4 2xl:px-16">
+        <h1 className=""> Navigation </h1>
+        <div>
+          <ul className="hidden md:flex">
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Vision</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">Vision</li>
             </Link>
           </ul>
+          <div className="md:hidden">
+            <BiCode size={30} />
           </div>
-        </div> 
+        </div>
+      </div>
     </div>
   )
 }
