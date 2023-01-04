771f852bdaacd603d42e3dbcecbef071aabef435
Gurpreet <gs109690@gmail.com>
2023-01-04 16:06:45 -0600

    Created new component Main.jsx for home page content

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen">
+      <div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
