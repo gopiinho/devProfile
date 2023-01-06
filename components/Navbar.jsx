ab39f3e221ed8d924ad1834df821219c444872e8
Gurpreet <gs109690@gmail.com>
2023-01-05 21:46:36 -0600

    Changed navbar layout and global css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,24 +15,26 @@ const Navbar = () => {
     setNav(!nav)
   }
   return (
-    <div className="fixed w-full h-25 shadow-xl z-[100]">
-      <div className="flex justify-between items-center w-full h-full p-4 2xl:px-16">
-        <h1 className=""> Gopinho </h1>
+    <div className="fixed w-full h-25 shadow-md  shadow-red-300 z-[100]">
+      <div className="flex justify-between items-center w-full h-full p-12 2xl:px-16 ">
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+              <li className="ml-10 text-lg  hover:border-b">home</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+              <li className="ml-10 text-lg  hover:border-b">about</li>
+            </Link>
+          </ul>
+        </div>
+        <h1> Gopinho </h1>
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-lg  hover:border-b">projects</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">
-                Projects
-              </li>
-            </Link>
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Vision</li>
+              <li className="ml-10 text-lg  hover:border-b">vision</li>
             </Link>
           </ul>
           <div onClick={handleNav} className="md:hidden">
