db1090be1e18822109b30323f1224e95c0557261
Gurpreet Singh <gs109690@gmail.com>
2023-01-12 21:48:07 -0600

    Projects Items are created using new component named ProjectsItems

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import sibylImage from "../public/assets/projects/sibyl.png";
+import cyberImage from "../public/assets/projects/cyber.png";
 import Link from "next/link";
+import ProjectsItems from "./ProjectsItems";
 
 const Projects = () => {
   return (
@@ -13,53 +15,19 @@ const Projects = () => {
           </h2>
         </div>
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="relative flex items-center justify-center h-auto w-full group rounded-sm p-1 opacity-75 hover:opacity-100 transition duration-700">
-            <div className="absolute -inset-0.5 opacity-75 bg-gradient-to-r from-[#ff2a6d] to-[#05d9e8] rounded-lg blur group-hover:opacity-0 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-            <Image
-              className="relative rounded-md group-hover:opacity-10 transition duration-500"
-              src={sibylImage}
-              alt="/"
-            />
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-cyber">
-              <h3 className="text-2xl tracking-wider text-center">
-                NFT Mint Page
-              </h3>
+          <ProjectsItems
+            title="NFT MINT PAGE"
+            backgroundImg={sibylImage}
+            stack="ReactJS / Solidity"
+            projectUrl="/"
+          />
 
-              <p className="pb-4 pt-2 text-white text-center">
-                React JS / Solidty
-              </p>
-
-              <Link href="/">
-                <p className="neonH2 text-center py-2 mx-8 rounded-md bg-white text-[#ff2a6d] text-lg cursor-pointer opacity-75 hover:opacity-100 transition duration-00">
-                  Visit
-                </p>
-              </Link>
-            </div>
-          </div>
-
-          <div className="relative flex items-center justify-center h-auto w-full group rounded-sm p-1 opacity-75 hover:opacity-100 transition duration-700">
-            <div className="absolute -inset-0.5 opacity-75 bg-gradient-to-r from-[#ff2a6d] to-[#05d9e8] rounded-lg blur group-hover:opacity-0 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-            <Image
-              className="relative rounded-md group-hover:opacity-10 transition duration-500"
-              src={sibylImage}
-              alt="/"
-            />
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-cyber">
-              <h3 className="text-2xl tracking-wider text-center">
-                NFT Mint Page
-              </h3>
-
-              <p className="pb-4 pt-2 text-white text-center">
-                React JS / Solidty
-              </p>
-
-              <Link href="/">
-                <p className="neonH2 text-center py-2 mx-8 rounded-md bg-white text-[#ff2a6d] text-lg cursor-pointer opacity-75 hover:opacity-100 transition duration-00">
-                  Visit
-                </p>
-              </Link>
-            </div>
-          </div>
+          <ProjectsItems
+            title="NFT MARKETPLACE"
+            backgroundImg={cyberImage}
+            stack="NextJS / Solidity"
+            projectUrl="/"
+          />
         </div>
       </div>
     </div>
---
+++ components/ProjectsItems.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const ProjectsItems = ({ title, backgroundImg, stack, projectUrl }) => {
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full group rounded-sm p-1 opacity-75 hover:opacity-100 transition duration-700">
+      <div className="absolute -inset-0.5 opacity-75 bg-gradient-to-r from-[#ff2a6d] to-[#05d9e8] rounded-lg blur group-hover:opacity-0 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+      <Image
+        className="relative rounded-md group-hover:opacity-10 transition duration-500"
+        src={backgroundImg}
+        alt="/"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-cyber">
+        <h3 className="text-2xl tracking-wider text-center">{title}</h3>
+
+        <p className="pb-4 pt-2 text-white text-center">{stack}</p>
+
+        <Link href={projectUrl}>
+          <p className="neonH2 text-center py-2 mx-8 rounded-md bg-white text-[#ff2a6d] text-lg cursor-pointer opacity-75 hover:opacity-100 transition duration-700">
+            Visit
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsItems;
