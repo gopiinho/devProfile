ca9222601fc67dcb63d2cce193b7afd549b4be40
Gurpreet <gs109690@gmail.com>
2023-01-17 12:16:49 -0600

    Contact page boiler

---
+++ components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16  w-full">
+        <div className="neonH2 flex items-center justify-center cursor-default">
+          <h2 className="text-3xl tracking-widest uppercase text-center my-12 font-cyber">
+            Contact
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Main from "../components/Main"
 import About from "../components/About"
 import Skills from "../components/Skills"
 import Projects from "../components/Projects"
+import Contact from "../components/Contact"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -29,6 +30,7 @@ export default function Home() {
       <Main />
       <Skills />
       <Projects />
+      <Contact />
     </>
   )
 }
