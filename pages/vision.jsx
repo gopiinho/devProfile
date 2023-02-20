b9438165099575572e04ba4bbd5450b4aa69bf44
Gurpreet <gs109690@gmail.com>
2023-02-20 05:31:54 -0600

    Vision page started

--- pages/vision.jsx
+++ pages/vision.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { motion as m } from "framer-motion"
-
+import storyBg from "../public/assets/backgrounds/cyber.gif"
 const vision = () => {
   return (
     <m.div
@@ -11,8 +11,8 @@ const vision = () => {
       className="w-full h-screen text-center font-robot "
     >
       <div className="flex flex-col justify-between lg:flex-row md:flex-row h-full sm:flex-col md:h-screen  sm:h-screen sm:justify-center md:justify-center lg:justify-center font-robot pt-20 sm:pt-20 md:pt-20">
-        <div className="p-4 bg-[#e2dddf]  lg:w-1/3 md:w-full h-full sm:w-full ">
-          <div className="flex lg:flex-col md:flex-col sm:flex-row sm:w-full justify-between h-full py-24 text-slate-700 ">
+        <div className="p-4 bg-[#e2dddf] lg:w-1/3 md:w-full h-full sm:w-full">
+          <div className="flex lg:flex-col md:flex-col sm:flex-row sm:w-full justify-between h-full py-24  text-slate-700 ">
             <h1 className="">Storyteller</h1>
             <p>Test</p>
           </div>
@@ -23,8 +23,8 @@ const vision = () => {
             <p>Test</p>
           </div>
         </div>
-        <div className="p-4 bg-[#ff6e27]  lg:w-1/3 md:w-full h-full sm:w-full">
-          <div className="flex lg:flex-col md:flex-col sm:flex-row sm:w-full justify-between h-full py-24 text-slate-700">
+        <div className="p-4 bg-[#ADD8E6]  lg:w-1/3 md:w-full h-full sm:w-full">
+          <div className="flex lg:flex-col md:flex-col sm:flex-row sm:w-full justify-between h-full py-24">
             <h1>Designer</h1>
             <p>Test</p>
           </div>
