4ba6d34651e1ea274571ac7e62bbc8ebbea8e3f4
Gurpreet <gs109690@gmail.com>
2023-04-23 01:36:27 -0500

    Updated image extension to fix build error

--- public/assets/backgrounds/index.js
+++ public/assets/backgrounds/index.js
@@ -6,7 +6,7 @@ import projectBG from "./projects.jpeg"
 import contactBG from "./contact.png"
 import stackBG from "./stack.png"
 import sibylBG from "./sibyl.gif"
-import profilePic from "./profile.JPG"
+import profilePic from "./profile.jpg"
 
 export {
   storyBG,
