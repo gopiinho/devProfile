6e9dac0a7611bf4e8996bd004cc1fc3983c7e042
Gurpreet <gs109690@gmail.com>
2023-04-24 22:25:19 -0500

    Updated the images and fixed crossOrigin error

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&family=Phudu&family=Roboto:wght@300&display=swap"
           rel="stylesheet"
