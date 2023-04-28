6b8fa25e85cd8976134e4c623aec5145215aa064
Gurpreet Singh <gs109690@gmail.com>
2023-04-27 19:43:47 -0500

    Minor changed to layput

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -16,10 +16,10 @@ const Skills = () => {
       <div className="flex flex-col w-full">
         <div className="flex flex-col sm:flex-row w-full h-1/2">
           <div
-            className="w-1/2 pr-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
+            className="w-1/2 group pr-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
             style={{ height: "calc(50vh - 75px)" }}
           >
-            <h2 className="writing-vertical h-full text-zinc-700 uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-300 px-2">
+            <h2 className="writing-vertical rotate-180 h-full text-zinc-700 uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-800 group-hover:text-white px-2">
               Design
             </h2>
             <div className="flex flex-col h-full items-center justify-between py-12">
@@ -35,7 +35,7 @@ const Skills = () => {
             </div>
           </div>
           <div
-            className="w-1/2 pl-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
+            className="w-1/2 group pl-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
             style={{ height: "calc(50vh - 75px)" }}
           >
             <div className="w-[60%] h-full py-8">
@@ -50,20 +50,19 @@ const Skills = () => {
               <p>Figma</p>
             </div>
 
-            <h2 className="writing-vertical h-full text-zinc-700 uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-300 px-2">
-              Design
+            <h2 className="writing-vertical h-full text-zinc-700 uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-800 group-hover:text-white px-2">
+              Web
             </h2>
           </div>
         </div>
         <div
-          className="w-full h-1/2 flex flex-col items-center hover:bg-zinc-300 hover:text-black transform duration-300"
+          className="w-full group h-1/2 flex flex-col items-center border-t border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
           style={{ height: "calc(50vh)" }}
         >
-          <div className="text-zinc-700 w-full uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-300 py-2">
-            Stack
-          </div>
           <div className="grid grid-cols-4 w-full h-full justify-center text-center">
-            <div className="flex items-center justify-center">Solidity</div>
+            <div className="flex items-center justify-center hover:bg-zinc-800 transform duration-300">
+              Solidity
+            </div>
             <div className="flex items-center justify-center">Typescript</div>
             <div className="flex items-center justify-center">Typescript</div>
             <div className="flex items-center justify-center">Tailwindcss</div>
@@ -71,6 +70,9 @@ const Skills = () => {
             <div className="flex items-center justify-center">Solidity</div>
             <div className="flex items-center justify-center">Solidity</div>
             <div className="flex items-center justify-center">Solidity</div>
+          </div>
+          <div className="w-full rotate-180 uppercase text-4xl text-zinc-700 text-center transform duration-300 cursor-default bg-zinc-800 group-hover:text-white py-2">
+            Stack
           </div>
         </div>
       </div>
