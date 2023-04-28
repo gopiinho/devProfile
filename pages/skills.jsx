2d27f21505b4c127c1a5d7f2a096db7eaf675499
Gurpreet Singh <gs109690@gmail.com>
2023-04-27 19:12:27 -0500

    Framework for the new page

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -11,28 +11,67 @@ const Skills = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3, ease: "easeOut" }}
-      className="w-full h-screen bg-zinc-800 pt-[75px] font-cyber"
+      className="w-full h-screen bg-zinc-800 pt-[75px] font-robot"
     >
       <div className="flex flex-col w-full">
         <div className="flex flex-col sm:flex-row w-full h-1/2">
           <div
-            className="w-1/2 sm:w-full flex justify-center items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
+            className="w-1/2 pr-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
             style={{ height: "calc(50vh - 75px)" }}
           >
-            Design
+            <h2 className="writing-vertical h-full text-zinc-700 uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-300 px-2">
+              Design
+            </h2>
+            <div className="flex flex-col h-full items-center justify-between py-12">
+              <p>Photoshop</p>
+              <p>Gimp</p>
+              <p>Figma</p>
+            </div>
+            <div className="w-[60%] h-full py-8">
+              <p>
+                This is just to check how big can this shit get and how can I re
+                arrange this. Probably would need to fix this.
+              </p>
+            </div>
           </div>
           <div
-            className="w-1/2 sm:w-full flex justify-center items-center hover:bg-zinc-300 hover:text-black transform duration-300"
+            className="w-1/2 pl-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
             style={{ height: "calc(50vh - 75px)" }}
           >
-            Webapps
+            <div className="w-[60%] h-full py-8">
+              <p>
+                This is just to check how big can this shit get and how can I re
+                arrange this. Probably would need to fix this.
+              </p>
+            </div>
+            <div className="flex flex-col h-full items-center justify-between py-12">
+              <p>Photoshop</p>
+              <p>Gimp</p>
+              <p>Figma</p>
+            </div>
+
+            <h2 className="writing-vertical h-full text-zinc-700 uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-300 px-2">
+              Design
+            </h2>
           </div>
         </div>
         <div
-          className="w-full h-1/2 flex justify-center items-center border-t border-zinc-500 hover:bg-zinc-300 hover:text-black transform duration-300"
+          className="w-full h-1/2 flex flex-col items-center hover:bg-zinc-300 hover:text-black transform duration-300"
           style={{ height: "calc(50vh)" }}
         >
-          Blockchain
+          <div className="text-zinc-700 w-full uppercase text-4xl text-center transform duration-300 cursor-default bg-zinc-300 py-2">
+            Stack
+          </div>
+          <div className="grid grid-cols-4 w-full h-full justify-center text-center">
+            <div className="flex items-center justify-center">Solidity</div>
+            <div className="flex items-center justify-center">Typescript</div>
+            <div className="flex items-center justify-center">Typescript</div>
+            <div className="flex items-center justify-center">Tailwindcss</div>
+            <div className="flex items-center justify-center">Node.js</div>
+            <div className="flex items-center justify-center">Solidity</div>
+            <div className="flex items-center justify-center">Solidity</div>
+            <div className="flex items-center justify-center">Solidity</div>
+          </div>
         </div>
       </div>
     </m.div>
