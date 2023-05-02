a7814326144dfd9e5ec5696d318e1e7202235054
Gurpreet Singh <gs109690@gmail.com>
2023-05-02 14:50:06 -0500

    Heading colour

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
             className="w-1/2 group pr-6 sm:w-full justify-between flex items-center border-r border-zinc-500 hover:bg-zinc-300 hover:text-zinc-700 transform duration-300"
             style={{ height: "calc(50vh - 75px)" }}
           >
-            <h2 className="writing-vertical font-cyber rotate-180 h-full text-zinc-700 uppercase text-3xl text-center transform duration-300 cursor-default bg-zinc-800 group-hover:text-[#ff577d] px-2">
+            <h2 className="writing-vertical font-cyber rotate-180 h-full text-zinc-500 uppercase text-3xl text-center transform duration-300 cursor-default bg-[#3c3d42] group-hover:text-[#ff577d] px-2">
               Blockchain
             </h2>
             <div className="grid grid-cols-1 font-phudu w-[40%] h-full justify-center text-center font-semibold uppercase tracking-lighter ">
@@ -113,7 +113,7 @@ const Skills = () => {
               </div>
             </div>
 
-            <h2 className="writing-vertical font-cyber h-full text-zinc-700 uppercase text-3xl text-center transform duration-300 cursor-default bg-zinc-800 group-hover:text-[#ff577d] px-2">
+            <h2 className="writing-vertical font-cyber h-full text-zinc-500 uppercase text-3xl text-center transform duration-300 cursor-default bg-[#3c3d42] group-hover:text-[#ff577d] px-2">
               Web
             </h2>
           </div>
@@ -196,7 +196,7 @@ const Skills = () => {
               <div className="devicon-docker-plain text-3xl"></div>
             </Link>
           </div>
-          <div className="w-full rotate-180 font-cyber uppercase text-3xl text-zinc-700 text-center transform duration-300 cursor-default bg-zinc-800 group-hover:text-[#ff577d] py-2">
+          <div className="w-full rotate-180 font-cyber uppercase text-3xl text-zinc-500 text-center transform duration-300 cursor-default bg-[#3c3d42] group-hover:text-[#ff577d] py-2">
             Stack
           </div>
         </div>
