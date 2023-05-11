17bd5515d066cd17362ca9105e4a2f8b49fc0435
Gurpreet Singh <gs109690@gmail.com>
2023-05-10 23:50:37 -0500

    Update: Main widths

--- components/Main.jsx
+++ components/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
     >
       <div className="flex lg:flex-col md:flex-col sm:flex-col w-full h-full justify-between">
         <div className="flex h-full lg:flex-row md:flex-row sm:flex-col">
-          <div className="flex flex-col gap-4 m-1 p-3 font-electro text-left bg-[#ff2a6d]/10 border border-[#ff2a6d]">
+          <div className="flex lg:w-[40%] md:w-[40%] sm:w-full flex-col gap-4 m-1 p-3 font-electro text-left bg-[#ff2a6d]/10 border border-[#ff2a6d]">
             <h1 className="text-4xl font-cyber text-[#ff2a6d] mb-8">Profile</h1>
             <Image
               src={avatar}
@@ -46,12 +46,17 @@ const Main = () => {
               <h2 className="text-2xl tracking-tighter flex justify-between">
                 Alias: <span className="text-right">Gopinho</span>
               </h2>
-              <p className="text-lg mt-4">
-                I make smart contracts and webapps.
-              </p>
             </div>
             <Link
-              href="https://github.com/igopib"
+              href="https://drive.google.com/file/d/1wM0f_5Z0fP6SfMVMZdNO6G_GSZCZrhn4/view?usp=share_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap-4 py-4 m-1 h-full lg:text-4xl md:text-2xl sm:text-xl items-center justify-center bg-[#ff2a6d]/30 hover:bg-[#ff2a6d]/60 border border-[#ff2a6d] transform duration-300"
+            >
+              blockchain cv
+            </Link>
+            <Link
+              href="https://drive.google.com/file/d/1wM0f_5Z0fP6SfMVMZdNO6G_GSZCZrhn4/view?usp=share_link"
               target="_blank"
               rel="noopener noreferrer"
               className="flex gap-4 py-4 m-1 h-full lg:text-4xl md:text-2xl sm:text-xl items-center justify-center bg-[#ff2a6d]/30 hover:bg-[#ff2a6d]/60 border border-[#ff2a6d] transform duration-300"
@@ -59,7 +64,7 @@ const Main = () => {
               cv
             </Link>
           </div>
-          <div className="w-full flex flex-col gap-4 m-1 p-3 font-electro text-left bg-[#ff2a6d]/10 border border-[#ff2a6d]"></div>
+          <div className="lg:w-[60%] md:w-[60%] sm:w-full flex flex-col gap-4 m-1 p-3 font-electro text-left bg-[#ff2a6d]/10 border border-[#ff2a6d]"></div>
         </div>
         <div>
           <div className="w-full lg:text-3xl md:text-2xl sm:text-sm">
